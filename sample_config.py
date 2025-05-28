fb3174790ac658d6336f34d4eb43a5a050940ff7
Farook13 <umarulfarook787@gmail.com>
2025-05-28 11:09:52 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -6,44 +6,39 @@ class Config(object):
     # Get a bot token from botfather
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7693803634:AAFIWfW8gfzMYv-G5-I9hAnge1mYFVkspio")
 
-
     # Get from my.telegram.org (or @UseTGXBot)
     API_ID = int(os.environ.get("API_ID", 12618934))
-
 
     # Get from my.telegram.org (or @UseTGXBot)
     API_HASH = os.environ.get("API_HASH", "49aacd0bc2f8924add29fb02e20c8a16")
     
-    
     # Database URL from https://cloud.mongodb.com/
     DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://batman13:batman13@batman.sawvl.mongodb.net/?retryWrites=true&w=majority&appName=batman")
-
 
     # Your database name from mongoDB
     DATABASE_NAME = str(os.environ.get("DATABASE_NAME", "batman"))
 
-
     # ID of users that can use the bot commands
-    AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "5032034594").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "5032034594").split())
 
-
-    # To save user details (Usefull for getting userinfo and total user counts)
+    # To save user details (Useful for getting userinfo and total user counts)
     # May reduce filter capacity :(
     # Give yes or no
     SAVE_USER = os.environ.get("SAVE_USER", "no").lower()
 
+    # To record start time of bot
+    BOT_START_TIME = time.time()
+
+    # OPTIONAL - To set alternate BOT COMMANDS
+    ADD_FILTER_CMD = os.environ.get("ADD_FILTER_CMD", "add")
+    DELETE_FILTER_CMD = os.environ.get("DELETE_FILTER_CMD", "del")
+    DELETE_ALL_CMD = os.environ.get("DELETE_ALL_CMD", "delall")
+    CONNECT_COMMAND = os.environ.get("CONNECT_COMMAND", "connect")
+    DISCONNECT_COMMAND = os.environ.get("DISCONNECT_COMMAND", "disconnect")
 
     # Go to https://dashboard.heroku.com/account, scroll down and press Reveal API
     # To check dyno status
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "")
-
-
-    # OPTIONAL - To set alternate BOT COMMANDS
-    ADD_FILTER_CMD = os.environ.get("ADD_FILTER_CMD", "add")
-    DELETE_FILTER_CMD = os.environ.get("DELETE_FILTER_CMDD", "del")
-    DELETE_ALL_CMD = os.environ.get("DELETE_ALL_CMDD", "delall")
-    CONNECT_COMMAND = os.environ.get("CONNECT_COMMANDD", "connect")
-    DISCONNECT_COMMAND = os.environ.get("DISCONNECT_COMMANDD", "disconnect")
 
 
     # To record start time of bot
